e41cf55ea19376317287847c42e11f931343754c
Mustafa Dal <you@example.com>
2019-02-02 21:15:03 +0300

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middilewares/logger.middleware';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('cats')
+  }
+}
